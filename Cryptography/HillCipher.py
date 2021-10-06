66dcf037e323355ac6a6e86dc11be56b05eed75b
s-bot12 <lkns0989@gmail.com>
2021-10-06 12:33:57 +0900

    21.10.06

--- Cryptography/HillCipher.py
+++ Cryptography/HillCipher.py
@@ -1,51 +1,112 @@
 import numpy as np
 
-def is_number(key) : #key 값을 검증
-	if(len(key) <= 0) :
+def make_inverse(list) : #mod 26에 대한 역행렬 만드는 함수
+	result = []
+	inverse = (list[0] * list[3]) - (list[1] * list[2])
+	if(inverse < 0) :
+		inverse = inverse % 26  
+	for i in range(1,26) :
+		if (inverse * i) % 26 == 1 :
+			inverse = i
+			break
+	list[0], list[3] = list[3], list[0]
+	list[1] = 26 - list[1]
+	list[2] = 26 - list[2]
+	for i in range(len(list)) :
+		list[i] = (inverse * list[i]) % 26
+	return list
+	
+def list_chunk(list, n) : #n개씩 하위 리스트로 분할
+	return [list[i:i+n] for i in range(0, len(list), n)]
+
+def is_key(key) : #예외처리 및 gcd 기반 key 값을 검증
+	if(len(key) != 4) :
 		return 0
 	for i in key :
 		if(type(i) != int) :
 			return 0
-		elif(i > 25 or i < 0) :
-			return 0	
-	return 1
+
+	inverse = (key[0] * key[3]) - (key[1] * key[2])
+
+	if(inverse < 0) :
+		inverse = inverse % 26  
+	for i in range(1,26) :
+		if (inverse * i) % 26 == 1 :
+			return 1
+	return 0
 
 def encryption(key, text) : #암호화 알고리즘
-	key_index = 0
+	text_array = []
+	result = []
+	index = 0 #text에서 알파벳만 바꿔주는 index
+	switch = 0 #마지막 text에 x를 추가했는지 구분
+	
 	for i in range(len(text)) :
 		if(ord(text[i].upper()) >= 65 and ord(text[i].upper()) <= 90) :
-			text[i] = ord(text[i].upper()) + (ord(key[key_index%len(key)].upper()) - 65) 
-			key_index += 1
-			while(text[i] > 90) :
-				text[i] -= 26
-			text[i] = chr(text[i])
-	return "".join(text)
+			text_array.append(ord(text[i].upper()) - 65)
+
+	if len(text_array) % 2 == 1 : #text가 홀수이면 x 하나를 넣어줌
+		text_array.append(23)
+		switch = 1 
+
+	text_array = np.array(list_chunk(text_array, 2))
+	key_array = np.array(list_chunk(key, 2))
+
+	text_array = ((text_array @ key_array) % 26) + 65 #2차원 배열 곱(내적)
+
+	for i in range(len(text_array)) :
+		result.append(chr(text_array[i][0]))
+		result.append(chr(text_array[i][1]))
+		
+	for i in range(len(text)) :
+		if(ord(text[i].upper()) >= 65 and ord(text[i].upper()) <= 90) :
+			text[i] = result[index]
+			index += 1
+
+	if switch == 1 :
+		text.append(result[index])
+		
+	return ''.join(text)
 
 def decryption(key, text) : #복호화 알고리즘
-	key_index = 0
+	text_array = [];
+	result = [];
+	index = 0
+	
 	for i in range(len(text)) :
-		if(ord(text[i].upper())>= 65 and ord(text[i].upper()) <= 90) :
-			text[i] = ord(text[i].upper()) - (ord(key[key_index%len(key)].upper()) - 65) 
-			key_index += 1
-			while(text[i] < 65) :
-				text[i] += 26
-			text[i] = chr(text[i])
-	return "".join(text)
+		if(ord(text[i].upper()) >= 65 and ord(text[i].upper()) <= 90) :
+			text_array.append(ord(text[i].upper()) - 65)
+
+	text_array = np.array(list_chunk(text_array, 2))	
+	key_array = make_inverse(key)
+	key_array = np.array(list_chunk(key, 2))
+
+	text_array = ((text_array @ key_array) % 26) + 65 #2차원 배열 곱(내적)
+
+	for i in range(len(text_array)) :
+		result.append(chr(text_array[i][0]))
+		result.append(chr(text_array[i][1]))
+
+	for i in range(len(text)) :
+		if(ord(text[i].upper()) >= 65 and ord(text[i].upper()) <= 90) :
+			text[i] = result[index]
+			index += 1
+
+	return ''.join(text)
 
 
 
 print("====Hill Cipher====\n")
 
-
 while True :
-	print("\n---2x2 Key를 입력해주세요!(26 미만 숫자만 사용 가능)---\n")
-	print("예시) 2,1,3,5 입력 시\n\t 2 1 \n\t 3 5 \n")
+	print("\n---2x2 Key를 입력해주세요! GCD(ad-bc, 26) = 1을 만족해야함---\n")
+	print("예시) 5,8,17,3 입력 시\n\t 5  8 \n\t 17 3 \n")
 	try :
 		key = list(map(int, input().split(',')))
 	except :
 		print("숫자 이외의 값은 넣을 수 없습니다! (종료)")
 		exit(1)
-	if(is_number(key) == 1) :
+	if(is_key(key) == 1) :
 		break;
 			
 print("\n---1,2중 선택해주세요! (1-암호화 2-복호화)---\n")
@@ -73,19 +134,10 @@ if(select == 1) :
 else :
 	text = decryption(key, list(text))
 
+print("\n----------\n" + text + "\n----------\n")
+make_f = open("result.txt", 'w')
+make_f.write(text)
+print("\n---result.txt로 결과 파일이 생성되었습니다!---\n")
+make_f.close()
+open_f.close()
 
-#print("\n----------\n" + text + "\n----------\n")
-#make_f = open("result.txt", 'w')
-#make_f.write(text)
-#print("\n---result.txt로 결과 파일이 생성되었습니다!---\n")
-#make_f.close()
-#open_f.close()
-
-
-#test_a = np.array([[12,4],[4,19],[12,4],[0,19],[4,8],[6,7],[19,14],[2,11],[14,2],[10,23]])
-#test_b = np.array([[7,3],[2,5]])
-#result = ((test_a @ test_b) % 26) + 65
-#for i in range(len(result)) :
-#	print(chr(result[i][0]) + ' ' + chr(result[i][1]))
-
-#print(chr(result[0][1] + 65))
